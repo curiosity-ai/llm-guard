16b8c0cc9397b7245c9a1d25e77121220ddb2ce9
theolivenbaum <rafael@curiosity.ai>
2024-11-18 03:42:43 +0100

    implement unicode homoglyphs filtering, add variations to prompts

--- LLM.Guard.Prepare/Program.cs
+++ LLM.Guard.Prepare/Program.cs
@@ -8,11 +8,19 @@ using System.Numerics;
 
 Console.WriteLine(Directory.GetCurrentDirectory());
 
-var forbiddenQuestionsPath = Path.Combine(Directory.GetCurrentDirectory(), "data", "jailbreak_llms", "data", "forbidden_question"); 
-var promptsPath            = Path.Combine(Directory.GetCurrentDirectory(), "data", "jailbreak_llms", "data", "prompts");
-var outputPath             = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory().Replace($"{Path.DirectorySeparatorChar}bin{Path.DirectorySeparatorChar}", "§").Split(new char[] { '§' })[0], "..", "LLM.Guard", "Resources"));
+var simpleJailbreakPromptsPath = Path.Combine(Directory.GetCurrentDirectory(), "data", "simple", "jailbreak-variations.txt"); 
+var forbiddenQuestionsPath     = Path.Combine(Directory.GetCurrentDirectory(), "data", "jailbreak_llms", "data", "forbidden_question"); 
+var promptsPath                = Path.Combine(Directory.GetCurrentDirectory(), "data", "jailbreak_llms", "data", "prompts");
+var outputPath                 = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory().Replace($"{Path.DirectorySeparatorChar}bin{Path.DirectorySeparatorChar}", "§").Split(new char[] { '§' })[0], "..", "LLM.Guard", "Resources"));
 
 Console.WriteLine(outputPath);
+
+var simpleJailbreakPrompts = File.ReadAllLines(simpleJailbreakPromptsPath)
+    .Select(f => new Prompt(f.Trim('"', '\n', ' ', '\r'), true))
+    .DistinctBy(q => q.prompt)
+    .ToArray();
+
+Console.WriteLine($"Read {simpleJailbreakPrompts.Length:n0} simple jailbreak prompts");
 
 var forbiddenQuestions = Directory.GetFiles(forbiddenQuestionsPath, "*.csv")
     .SelectMany(f =>
@@ -29,22 +37,20 @@ var forbiddenQuestions = Directory.GetFiles(forbiddenQuestionsPath, "*.csv")
 
 Console.WriteLine($"Read {forbiddenQuestions.Length:n0} forbidden questions");
 
+//var jailbreakPrompts = Directory.GetFiles(promptsPath, "jailbreak*.csv")
+//    .SelectMany(f =>
+//    {
+//        using (var reader = new StreamReader(f))
+//        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+//        {
+//            var records = csv.GetRecords<Prompt>();
+//            return records.ToArray();
+//        }
+//    })
+//    .DistinctBy(q => q.prompt)
+//    .ToArray();
 
-
-var jailbreakPrompts = Directory.GetFiles(promptsPath, "jailbreak*.csv")
-    .SelectMany(f =>
-    {
-        using (var reader = new StreamReader(f))
-        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-        {
-            var records = csv.GetRecords<Prompt>();
-            return records.ToArray();
-        }
-    })
-    .DistinctBy(q => q.prompt)
-    .ToArray();
-
-Console.WriteLine($"Read {jailbreakPrompts.Length:n0} jailbreak prompts");
+//Console.WriteLine($"Read {jailbreakPrompts.Length:n0} jailbreak prompts");
 
 var normalPrompts = Directory.GetFiles(promptsPath, "regular*.csv")
     .SelectMany(f =>
@@ -70,27 +76,31 @@ Console.WriteLine($"Encoding forbidden questions");
 var forbiddenQuestionsType    = forbiddenQuestions.ToDictionary(f => f, f => Enum.Parse<PromptType>("ForbiddenQuestion_" + f.content_policy_name.Replace(" ", "_")));
 var forbiddenQuestionsEncoded = forbiddenQuestions.ToDictionary(f => f, f => new EncodedPrompt(encoder.Encode([f.question])[0], forbiddenQuestionsType[f]));
 
-Console.WriteLine($"Encoding jailbreak prompts"); 
-var jailbreakPromptsEncoded = jailbreakPrompts.ToDictionary(f => f, f => new EncodedPrompt(encoder.Encode([f.prompt])[0], PromptType.Jailbreak));
+//Console.WriteLine($"Encoding jailbreak prompts");
+//var jailbreakPromptsEncoded = jailbreakPrompts.ToDictionary(f => f, f => new EncodedPrompt(encoder.Encode([f.prompt])[0], PromptType.Jailbreak));
+
+Console.WriteLine($"Encoding simple jailbreak prompts");
+var simpleJailbreakPromptsEncoded = simpleJailbreakPrompts.ToDictionary(f => f, f => new EncodedPrompt(encoder.Encode([f.prompt])[0], PromptType.Jailbreak));
 
 //Console.WriteLine($"Encoding normal prompts "); 
 //var normalPromptsEncoded = normalPrompts.ToDictionary(f => f, f => new EncodedPrompt(encoder.Encode([f.prompt])[0], f.prompt, PromptType.Jailbreak));
 
-using (var output = File.Open(Path.Combine(outputPath, "prompts.bin"), FileMode.Create))
-{
-    var all = forbiddenQuestionsEncoded.Values
-        .Concat(jailbreakPromptsEncoded.Values)
-        //.Concat(normalPromptsEncoded.Values)
-        .ToArray();
-    
-    await MessagePackSerializer.SerializeAsync(output, all);
-}
+var allPrompts = forbiddenQuestionsEncoded.Values
+                    //.Concat(jailbreakPromptsEncoded.Values)
+                    .Concat(simpleJailbreakPromptsEncoded.Values)
+                    //.Concat(normalPromptsEncoded.Values)
+                    .ToArray();
+
+var reverseMap = forbiddenQuestionsEncoded.Select(kv => (t: kv.Key.question, p:kv.Value))
+                        //.Concat(jailbreakPromptsEncoded.Select(kv => (t: kv.Key.prompt, p: kv.Value)))
+                        .Concat(simpleJailbreakPromptsEncoded.Select(kv => (t: kv.Key.prompt, p: kv.Value)))
+                    .ToDictionary(d => d.p,  d => d.t);
 
 var parameters = new SmallWorld<EncodedPrompt, float>.Parameters()
 {
     EnableDistanceCacheForConstruction = true,
     InitialDistanceCacheSize = 1_000_000,
-    InitialItemsSize = forbiddenQuestions.Length + jailbreakPrompts.Length + normalPrompts.Length,
+    InitialItemsSize = forbiddenQuestions.Length + /*jailbreakPrompts.Length +*/ + simpleJailbreakPrompts.Length + normalPrompts.Length,
     M = 20,
     ConstructionPruning = 200,
     LevelLambda = 1 / Math.Log(40),
@@ -99,134 +109,116 @@ var parameters = new SmallWorld<EncodedPrompt, float>.Parameters()
 };
 
 var graph = new SmallWorld<EncodedPrompt, float>(DistanceHelper.EncodedPromptDistance, DefaultRandomGenerator.Instance, parameters);
-graph.AddItems(forbiddenQuestionsEncoded.Values.ToArray());
-graph.AddItems(jailbreakPromptsEncoded.Values.ToArray());
-//graph.AddItems(normalPromptsEncoded.Values.ToArray());
+graph.AddItems(allPrompts);
 
 using (var output = File.Open(Path.Combine(outputPath, "hnsw-graph.bin"), FileMode.Create))
 {
     graph.SerializeGraph(output);
 }
 
-var scoresForbidden = new Dictionary<ForbiddenQuestion, (float, PromptType)>();
-var scoresJailbreak = new Dictionary<Prompt, (float, PromptType)>();
-var scoresNormal    = new Dictionary<Prompt, (float, PromptType)>();
+using (var output = File.Open(Path.Combine(outputPath, "prompts.bin"), FileMode.Create))
+{
+    await MessagePackSerializer.SerializeAsync(output, graph.Items.ToArray());
+}
+
+var scoresForbidden       = new Dictionary<ForbiddenQuestion, (float, PromptType)>();
+var scoresJailbreak       = new Dictionary<Prompt, (float, PromptType)>();
+var scoresSimpleJailbreak = new Dictionary<Prompt, (float, PromptType)>();
+var scoresNormal          = new Dictionary<Prompt, (float, PromptType)>();
 
 Console.WriteLine("Testing predictions, please wait...");
-int c = 0;
-foreach (var p in forbiddenQuestions)
+var finalStats = new List<string>();
+for (float f = 0.01f; f <= 0.19f; f+= 0.01f)
 {
-    var encoded = encoder.Encode([p.question])[0];
-    var closest = graph.KNNSearch(new EncodedPrompt(encoded, PromptType.Normal), 50);
-    var best = closest.OrderBy(kv => kv.Distance).First();
-    scoresForbidden[p] = (best.Distance, best.Item.PromptType);
-    if (c++ % 50 == 0) Console.WriteLine($"Forbidden Questions: At {c:n0} of {forbiddenQuestions.Length:n0}");
-}
+    int correct = 0, wrong = 0;
 
-c = 0;
-Random.Shared.Shuffle(jailbreakPrompts);
-foreach (var p in jailbreakPrompts.Take(300))
-{
-    var encoded = encoder.Encode([p.prompt])[0];
-    var closest = graph.KNNSearch(new EncodedPrompt(encoded, PromptType.Normal), 50);
-    var best = closest.OrderBy(kv => kv.Distance).First();
-    scoresJailbreak[p] = (best.Distance, best.Item.PromptType);
-    if (c++ % 50 == 0) Console.WriteLine($"Jailbreaks: At {c:n0} of {jailbreakPrompts.Length:n0}");
-}
-
-c = 0;
-Random.Shared.Shuffle(normalPrompts);
-foreach (var p in normalPrompts.Take(300))
-{
-    var encoded = encoder.Encode([p.prompt])[0];
-    var closest = graph.KNNSearch(new EncodedPrompt(encoded, PromptType.Normal), 50);
-    var best = closest.OrderBy(kv => kv.Distance).First();
-    scoresNormal[p] = best.Distance < 0.2f ? (best.Distance, best.Item.PromptType) : (0, PromptType.Normal);
-    if (c++ % 50 == 0) Console.WriteLine($"Normal Prompts: At {c:n0} of {normalPrompts.Length:n0}");
-}
-
-Console.WriteLine("Done testing predictions.\n\n");
-
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Illegal_Activity),    PromptType.ForbiddenQuestion_Illegal_Activity);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Hate_Speech),         PromptType.ForbiddenQuestion_Hate_Speech);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Malware),             PromptType.ForbiddenQuestion_Malware);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Physical_Harm),       PromptType.ForbiddenQuestion_Physical_Harm);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Economic_Harm),       PromptType.ForbiddenQuestion_Economic_Harm);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Fraud),               PromptType.ForbiddenQuestion_Fraud);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Pornography),         PromptType.ForbiddenQuestion_Pornography);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Political_Lobbying),  PromptType.ForbiddenQuestion_Political_Lobbying);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Privacy_Violence),    PromptType.ForbiddenQuestion_Privacy_Violence);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Legal_Opinion),       PromptType.ForbiddenQuestion_Legal_Opinion);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Financial_Advice),    PromptType.ForbiddenQuestion_Financial_Advice);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Health_Consultation), PromptType.ForbiddenQuestion_Health_Consultation);
-PrintStats(scoresForbidden.Where(p => forbiddenQuestionsType[p.Key] == PromptType.ForbiddenQuestion_Gov_Decision),        PromptType.ForbiddenQuestion_Gov_Decision);
-
-PrintStatsPrompts(scoresJailbreak, PromptType.Jailbreak);
-
-PrintStatsNormals(scoresNormal);
-
-
-void PrintStats(IEnumerable<KeyValuePair<ForbiddenQuestion, (float score, PromptType type)>> scores, PromptType expected)
-{
-    int correct = 0, total = 0, wrong = 0;
-    float minScore = float.MaxValue;
-    float maxScore = float.MinValue;
-    foreach (var (source, pred) in scores)
+    foreach (var p in allPrompts)
     {
-        total++;
-        if(pred.type == expected)
+        var closest = graph.KNNSearch(p, 50);
+        var best    = closest.OrderBy(kv => kv.Distance).First();
+        var guess   = PromptType.Normal;
+
+        if (best.Distance <= f)
+        {
+            guess = best.Item.PromptType;
+        }
+
+        if (p.PromptType == guess)
         {
             correct++;
         }
         else
         {
-            minScore = Math.Min(minScore, pred.score);
-            maxScore = Math.Max(maxScore, pred.score);
             wrong++;
+            Console.WriteLine($"{p.PromptType} != {guess} for '{reverseMap[p]}'");
         }
     }
-    Console.WriteLine($"{expected} -> minScore: {minScore:n2} maxScore: {maxScore:n2} correct: {correct:n0} ({100f*correct/total:n1}%) wrong {wrong:n0} ({100f * wrong/ total:n1}%) total {total:n0}");
+    finalStats.Add($"{f:n2} -> C:{correct:n0} W: {wrong:n0} P: {100f * correct / allPrompts.Length:n1}");
 }
 
+finalStats.ForEach(l => Console.WriteLine(l));
 
-void PrintStatsPrompts(Dictionary<Prompt, (float score, PromptType type)> scores, PromptType expected)
-{
-    int correct = 0, total = 0, wrong = 0;
-    float minScore = float.MaxValue;
-    float maxScore = float.MinValue;
-    foreach (var (source, pred) in scores)
-    {
-        total++;
-        if (pred.type == expected)
-        {
-            correct++;
-        }
-        else
-        {
-            minScore = Math.Min(minScore, pred.score);
-            maxScore = Math.Max(maxScore, pred.score);
-            wrong++;
-        }
-    }
-    Console.WriteLine($"{expected} -> minScore: {minScore:n2} maxScore: {maxScore:n2} correct: {correct:n0} ({100f * correct / total:n1}%) wrong {wrong:n0} ({100f * wrong / total:n1}%) total {total:n0}");
-}
+//void PrintStats(IEnumerable<KeyValuePair<ForbiddenQuestion, (float score, PromptType type)>> scores, PromptType expected)
+//{
+//    int correct = 0, total = 0, wrong = 0;
+//    float minScore = float.MaxValue;
+//    float maxScore = float.MinValue;
+//    foreach (var (source, pred) in scores)
+//    {
+//        total++;
+//        if(pred.type == expected)
+//        {
+//            correct++;
+//        }
+//        else
+//        {
+//            minScore = Math.Min(minScore, pred.score);
+//            maxScore = Math.Max(maxScore, pred.score);
+//            wrong++;
+//        }
+//    }
+//    Console.WriteLine($"{expected} -> minScore: {minScore:n2} maxScore: {maxScore:n2} correct: {correct:n0} ({100f*correct/total:n1}%) wrong {wrong:n0} ({100f * wrong/ total:n1}%) total {total:n0}");
+//}
 
-void PrintStatsNormals(Dictionary<Prompt, (float score, PromptType type)> scores)
-{
-    int total = 0;
-    float minScore = float.MaxValue;
-    float maxScore = float.MinValue;
-    float avgScore = 0;
-    foreach (var (source, pred) in scores)
-    {
-        total++;
-        minScore = Math.Min(minScore, pred.score);
-        maxScore = Math.Max(maxScore, pred.score);
-        avgScore += pred.score;
-    }
-    avgScore /= total;
-    Console.WriteLine($"Normal Prompts: minScore = {minScore:n2} avgScore = {avgScore:n2} maxScore = {maxScore:n2}");
-}
+
+//void PrintStatsPrompts(Dictionary<Prompt, (float score, PromptType type)> scores, PromptType expected)
+//{
+//    int correct = 0, total = 0, wrong = 0;
+//    float minScore = float.MaxValue;
+//    float maxScore = float.MinValue;
+//    foreach (var (source, pred) in scores)
+//    {
+//        total++;
+//        if (pred.type == expected)
+//        {
+//            correct++;
+//        }
+//        else
+//        {
+//            minScore = Math.Min(minScore, pred.score);
+//            maxScore = Math.Max(maxScore, pred.score);
+//            wrong++;
+//        }
+//    }
+//    Console.WriteLine($"{expected} -> minScore: {minScore:n2} maxScore: {maxScore:n2} correct: {correct:n0} ({100f * correct / total:n1}%) wrong {wrong:n0} ({100f * wrong / total:n1}%) total {total:n0}");
+//}
+
+//void PrintStatsNormals(Dictionary<Prompt, (float score, PromptType type)> scores)
+//{
+//    int total = 0;
+//    float minScore = float.MaxValue;
+//    float maxScore = float.MinValue;
+//    float avgScore = 0;
+//    foreach (var (source, pred) in scores)
+//    {
+//        total++;
+//        minScore = Math.Min(minScore, pred.score);
+//        maxScore = Math.Max(maxScore, pred.score);
+//        avgScore += pred.score;
+//    }
+//    avgScore /= total;
+//    Console.WriteLine($"Normal Prompts: minScore = {minScore:n2} avgScore = {avgScore:n2} maxScore = {maxScore:n2}");
+//}
 
 
 
--- LLM.Guard/Predictor.cs
+++ LLM.Guard/Predictor.cs
@@ -1,6 +1,7 @@
 ﻿using HNSW.Net;
 using MessagePack;
 using SentenceTransformers.ArcticXs;
+using System.Text;
 
 namespace LLM.Guard;
 
@@ -10,11 +11,14 @@ public sealed class Predictor
     private readonly EncodedPrompt[] _items;
     private readonly SmallWorld<EncodedPrompt, float> _graph;
     private readonly SentenceEncoder _encoder;
+    private readonly Dictionary<char, char> _homoglyphs;
 
     private Predictor()
     {
-        using (var hnswGraph = ResourceLoader.OpenResource(typeof(Predictor).Assembly, "hnsw-graph.bin"))
-        using (var prompts   = ResourceLoader.OpenResource(typeof(Predictor).Assembly, "prompts.bin"))
+        using (var hnswGraph  = ResourceLoader.OpenResource(typeof(Predictor).Assembly, "hnsw-graph.bin"))
+        using (var prompts    = ResourceLoader.OpenResource(typeof(Predictor).Assembly, "prompts.bin"))
+        using (var homoglyphs = ResourceLoader.OpenResource(typeof(Predictor).Assembly, "homoglyphs.txt"))
+        using (var homoglyphsReader = new StreamReader(homoglyphs))
         {
             var parameters = new SmallWorld<EncodedPrompt, float>.Parameters()
             {
@@ -28,12 +32,17 @@ public sealed class Predictor
             _items = MessagePackSerializer.Deserialize<EncodedPrompt[]>(prompts);
             _graph = SmallWorld<EncodedPrompt, float>.DeserializeGraph(_items, DistanceHelper.EncodedPromptDistance, DefaultRandomGenerator.Instance, hnswGraph);
             _encoder = new SentenceTransformers.ArcticXs.SentenceEncoder();
+            _homoglyphs = homoglyphsReader.ReadToEnd().Split(['\r', '\n'])
+                            .Where(l => l.Length > 1 && l[0] != '#')
+                            .SelectMany(l => l.Skip(1).Select(c => (final: l[0], variation: c)))
+                            .DistinctBy(d => d.variation)
+                            .ToDictionary(d => d.variation, d => d.final);
         }
     }
 
-    public PromptType Predict(string text, int count = 50, float maxDistance = 0.2f)
+    public PromptType Predict(string text, int count = 50, float maxDistance = 0.1f, bool removeHomoglyphs = true)
     {
-        var encoded = _encoder.Encode([text])[0];
+        var encoded = _encoder.Encode([removeHomoglyphs ? RemoveHomoglyphs(text) : text])[0];
         var closest = _graph.KNNSearch(new EncodedPrompt(encoded, PromptType.Normal), count);
         var best = closest.OrderBy(kv => kv.Distance).First();
 
@@ -42,14 +51,31 @@ public sealed class Predictor
         return PromptType.Normal;
     }
 
-    public Prediction PredictWithScore(string text, int count = 50, float maxDistance = 0.2f)
+    public Prediction PredictWithScore(string text, int count = 50, float maxDistance = 0.1f, bool removeHomoglyphs = true)
     {
-        var encoded = _encoder.Encode([text])[0];
+        var encoded = _encoder.Encode([removeHomoglyphs ? RemoveHomoglyphs(text) : text])[0];
         var closest = _graph.KNNSearch(new EncodedPrompt(encoded, PromptType.Normal), count);
         var best = closest.OrderBy(kv => kv.Distance).First();
 
         if (best.Distance < maxDistance) return new (best.Item.PromptType, best.Distance);
 
         return new (PromptType.Normal, best.Distance);
+    }
+
+    public string RemoveHomoglyphs(string text)
+    {
+        var sb = new StringBuilder(text.Length);
+        foreach(var c in text)
+        {
+            if(_homoglyphs.TryGetValue(c, out var r))
+            {
+                sb.Append(r);
+            }
+            else
+            {
+                sb.Append(c);
+            }
+        }
+        return sb.ToString();
     }
 }
